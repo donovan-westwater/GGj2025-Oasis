0ae6cc5ff8c0122718cb7b0eee64c5a55e6a5d13
Bo Yuan Zhou <bz88@cornell.edu>
2025-01-25 14:55:01 -0800

    [Letter] Changing book prototype to be letters instead

---
+++ Assets/Book - Custom/scripts/Letter.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.Assertions;
+
+[ExecuteInEditMode]
+public class Letter : MonoBehaviour {
+    public GameObject[] Pages;
+    public int CurrentPageIndex = 0;
+    public bool IsLocked = true;
+
+    void Start()
+    {
+    }
+
+    public void Activate()
+    {
+        this.gameObject.SetActive(true);
+        SetCurrentPage(0);
+    }
+
+    public void Deactivate()
+    {
+        SetCurrentPage(0);
+        this.gameObject.SetActive(false);
+    }
+
+    public bool CanDecrementPage()
+    {
+        return CurrentPageIndex > 0;
+    }
+
+    public bool CanIncrementPage()
+    {
+        return CurrentPageIndex < Pages.Length - 1;
+    }
+
+    public void DecrementPage()
+    {
+        Assert.IsTrue(CanDecrementPage());
+        SetCurrentPage(CurrentPageIndex - 1);
+    }
+
+    public void IncrementPage()
+    {
+        Assert.IsTrue(CanIncrementPage());
+        SetCurrentPage(CurrentPageIndex + 1);
+    }
+
+    private void SetCurrentPage(int iPage)
+    {
+        if (CurrentPageIndex == iPage) return;
+
+        Pages[CurrentPageIndex].gameObject.SetActive(false);
+
+        CurrentPageIndex = iPage;
+
+        Pages[CurrentPageIndex].gameObject.SetActive(true);
+    }
+}
