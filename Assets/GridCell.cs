a5558a7ac2ceb5a973782fba034c2b270e3464a0
donovan-westwater <donovan.westwater@gmail.com>
2025-01-25 10:25:11 -0800

    Grid system basics have been setup

--- Assets/GridCell.cs
+++ Assets/GridCell.cs
@@ -6,6 +6,8 @@ public class GridCell : MonoBehaviour
 {
     [HideInInspector]
     public bool isSelected = false;
+    [HideInInspector]
+    public bool isHovered = false;
     Material mat;
     MeshRenderer renderer;
     Color defaultColor;
@@ -17,14 +19,25 @@ public class GridCell : MonoBehaviour
     }
 
     private void OnMouseEnter()
-    {        
+    {
+        isHovered = true;
+        GridSystem.hoveredCell = this;
+        Debug.Log(this.transform.parent.name + " " + this.name + " " + isHovered);
         mat.color = Color.red;
         renderer.material = mat;
         //Debug.Log(this.transform.parent.name+" "+this.name+" "+isSelected);
     }
     private void OnMouseExit()
     {
-        if (isSelected) return;
+        isHovered = false;
+        GridSystem.hoveredCell = null;
+        Debug.Log(this.transform.parent.name + " " + this.name + " " + isHovered);
+        if (isSelected)
+        {
+            mat.color = Color.green;
+            renderer.material = mat;
+            return;
+        }
         mat.color = this.defaultColor;
         renderer.material = mat;
         //Debug.Log("FREEDOM!");
@@ -32,12 +45,21 @@ public class GridCell : MonoBehaviour
     private void OnMouseDown()
     {
         isSelected = true;
+        if (this.transform.childCount > 0)
+            GridSystem.currentSelection = this.transform.GetChild(0).gameObject;
         mat.color = Color.green;
         renderer.material = mat;
     }
     private void OnMouseUp()
     {
         isSelected = false;
+        Debug.Log(this.transform.parent.name + " " + this.name + " MouseUP");
+        if (GridSystem.currentSelection != null && GridSystem.hoveredCell != null)
+        {
+            GridSystem.currentSelection.transform.SetParent(GridSystem.hoveredCell.transform);
+            GridSystem.currentSelection.transform.localPosition = new Vector3(0, 1f, 0);
+            GridSystem.currentSelection = null;
+        }
         mat.color = this.defaultColor;
         renderer.material = mat;
     }
--- Assets/GridSystem.cs
+++ Assets/GridSystem.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GridSystem : MonoBehaviour
 {
+    [HideInInspector]
+    public static GameObject currentSelection = null;
+    [HideInInspector]
+    public static GridCell hoveredCell = null;
     // Start is called before the first frame update
     void Start()
     {
